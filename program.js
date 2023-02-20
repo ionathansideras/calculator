1af9ead47324cbe842d623501953251e6440cba6
ionathansideras <119215357+ionathansideras@users.noreply.github.com>
2023-02-20 14:41:02 +0200

    Update program.js

--- program.js
+++ program.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', function(){
     
     let count = '';
+    let holder = 0;
     let buttons = document.querySelectorAll('#num');
-    
+    let bool;
+
+    document.querySelector('h1').innerHTML = holder;
+
     for( let i = 0; i < buttons.length; i++){
         buttons[i].onclick = function() {
             let number = buttons[i].innerHTML;
@@ -17,8 +21,32 @@ document.addEventListener('DOMContentLoaded', function(){
     };
 
     document.querySelector('.results').onclick = function(){
-        document.querySelector('h1').innerHTML = eval(count);
-        count = eval(count);
+
+        if (document.querySelector('h1').innerHTML == holder){
+            document.querySelector('h1').innerHTML == holder;
+        }
+
+        else if(count == "0/0" || count == "00/0" || count == "000/0" || count == "0000/0"){
+            document.querySelector('h1').innerHTML = "Error";
+        }
+
+        else if (document.querySelector('h1').innerHTML == count){
+            try {
+                eval(count);
+                count = eval(count);
+                document.querySelector('h1').innerHTML = count;
+              }
+            catch (error) {
+                document.querySelector('h1').innerHTML = "Error";
+              }           
+        } 
+
+        if (count == Infinity){
+            document.querySelector('h1').innerHTML = "Error";
+            count = '';
+        } 
+
+      
     };
     
     
